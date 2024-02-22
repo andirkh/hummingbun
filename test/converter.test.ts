c14c2b970ae426034dbe47090f66766b90feb137
andirkh <andirkh@icloud.com>
2024-02-22 18:35:15 +0700

    test: add test for converter

--- test/converter.test.ts
+++ test/converter.test.ts
@@ -46,6 +46,7 @@ describe("Convert Markdown to HTML", () => {
   test("unordered list", () => {
     expect(convertMdToHtml('- hello')).toBe('<li> hello</li>')
   })
+  
   test("ordered list", () => {
     const text: string = removeTabsInString(String.raw`
       1. hello
@@ -56,5 +57,53 @@ describe("Convert Markdown to HTML", () => {
     expect(convertMdToHtml(text)).toBe(
       '<ol><li>hello</li><li>llo</li><li>velo</li></ol>\n'
     )
+  })
+
+  test('codeblock', () => {
+    const text: string = removeTabsInString(`
+    \`\`\`
+    cd memo
+    \`\`\``)
+    expect(convertMdToHtml(text)).toBe('<pre><code>cd memo</code></pre>')
+  })
+
+  test('inline block', () => {
+    const text: string = removeTabsInString(`hello \`mycode\``)
+    expect(convertMdToHtml(text)).toBe('hello <code>mycode</code>')
+  })
+
+  test('images', () => {
+    const text: string = removeTabsInString(`![funny picture](http://funny.picture.com/asd)`)
+    expect(convertMdToHtml(text)).toBe(`<img alt="funny picture" src="http://funny.picture.com/asd">`)
+  })
+
+  test('links', () => {
+    const text: string = removeTabsInString(`[my picture link](http://funny.picture.com/asd)`)
+    expect(convertMdToHtml(text)).toBe(`<a href="http://funny.picture.com/asd">my picture link</a>`)
+  })
+
+  test('horizonal rules', () => {
+    const text: string = removeTabsInString(`---`)
+    expect(convertMdToHtml(text)).toBe(`<hr>\n`)
+  })
+
+  test('paragraph', () => {
+    const text: string = removeTabsInString(`
+    This is the first paragraph.
+    `)
+    expect(convertMdToHtml(text)).toBe(`<p>This is the first paragraph.</p>\n`)
+  })
+
+  test('multi line paragraph', () => {
+    const text: string = removeTabsInString(`
+    This is the first paragraph.
+
+    This is the second paragraph, 
+    which spans multiple lines.
+
+    And here is the third paragraph.
+    `)
+    expect(convertMdToHtml(text))
+      .toBe(`<p>This is the first paragraph.</p>\n<p>This is the second paragraph, </p>\n<p>which spans multiple lines.</p>\n<p>And here is the third paragraph.</p>\n`)
   })
 });
